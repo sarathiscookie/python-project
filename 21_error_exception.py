43d5c82d01d50b3d9dc202a65c3b65fc7f7fecde
Sarath T S <sarathiscookie@hotmail.com>
2022-01-26 15:32:19 +0530

    Error exception: try, except, else, finally

---
+++ 21_error_exception.py
@@ -0,0 +1,23 @@
+# Different types of errors
+# SyntaxError: Syntax not correct.
+# TypeError: Type is not correct.
+# ModuleNotFoundError: No module found.
+# NameError: Name is not found.
+# FileNotFoundError: File not found.
+# ValueError: Vale not found.
+# IndexError: Index not found.
+# KeyError: key not found.
+
+data = 5
+
+try:
+    division = data / 0
+    addition = data + '1'
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+except TypeError:
+    print("Can't add with string!")
+else:
+    print("Execute if not exemption!")
+finally:
+    print("Always execute!")     
