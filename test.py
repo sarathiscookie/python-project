b092518f22fb5b16c6ca49f5a79b3222fd28a340
Sarath T S <sarathiscookie@hotmail.com>
2022-03-23 01:30:49 +0530

    Deleted folder

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
+# Single responsibility principle: One class must be single responsibility.
+# Open-close principle: Open for extension and closed for modification.
+# Liskov substitution principle: Sub-class must substitutable for its super class
+# Interface segregation principle: Client shouldnot be forced to depend upon interface  
+# Dependency inversion principle: High-level module should not depend on low-level module. But both should depend on abstraction. Abstraction should not depend on details. Details should depend on abstraction.
+
+class 
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
 
 
 
