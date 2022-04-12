9c1f101efe1869ab59a40bf695aa03a3569346e7
Sarath T S <sarathiscookie@hotmail.com>
2022-04-12 15:14:06 +0530

    Project 4: Top 10 news.

---
+++ projects/4_top_10_news/main.py
@@ -0,0 +1,20 @@
+from turtle import clear
+import requests
+
+from bs4 import BeautifulSoup
+
+url_request = requests.get("https://news.ycombinator.com/news")
+# url_request.text
+
+all_news = BeautifulSoup(url_request.text, "html.parser")
+# print(soup.prettify())
+
+news_headlines = [news.getText() for news in all_news.find_all("a", class_="titlelink", limit=10)]
+# print(news_headlines)
+
+with open("/projects/4_top_10_news/news.txt", mode="w") as file:
+    for news_headline in news_headlines:
+        file.write(f"{news_headline} \n")
+
+print("Done! News headlines write in to a file.")
+
---
+++ projects/4_top_10_news/readme.md
@@ -0,0 +1,5 @@
+Web scrapping using beautiful soup.
+
+Get news data from url and store top 10 news headlines in to a txt file.
+
+https://news.ycombinator.com/news
