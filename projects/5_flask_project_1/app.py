bc5417e612734c63d2e7fb21f758a7f2b95ea020
Sarath T S <sarathiscookie@hotmail.com>
2022-04-21 17:27:27 +0530

    Project 5: Enabled debug true.

--- projects/5_flask_project_1/app.py
+++ projects/5_flask_project_1/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello I am Flask!!"
+    return "Hello There! I am Flask!!" 
 
 if __name__ == "__main__":
-    app.run()    
+    app.run(debug=True)    
