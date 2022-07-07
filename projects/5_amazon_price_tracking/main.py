e8f44a4d5a663f05c49c87058ac57b5529333eab
Sarath T S <sarathiscookie@hotmail.com>
2022-07-07 15:22:51 +0530

    Project 5: Price tracking. Disabled SMTP functionality.

--- projects/5_amazon_price_tracking/main.py
+++ projects/5_amazon_price_tracking/main.py
@@ -21,23 +21,28 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "lxml")
 #print(soup.prettify())
 
-result = soup.find(class_ ="a-offscreen").get_text()
-#print(result) #$36.38
+price_with_currency = soup.find(class_ ="a-offscreen").get_text()
+#print(price_with_currency) #$36.38
 
-price_without_currency = result.split("$")[1]
+product_title = soup.find(id = "productTitle").get_text()
+#product_title.strip() #BELLA Classic Rotating Non-Stick Belgian Waffle Maker, Perfect 1" Thick Waffles, PFOA Free Non Stick Coating & Removable Drip Tray for Easy Clean Up, Browning Control, Black
+
+price_without_currency = price_with_currency.split("$")[1]
 price_as_float = float(price_without_currency)
 
 if price_as_float < float(config('TARGET_PRICE')):
-
+    print(f"Amount is less than target price. Product title: {product_title.strip()}. Todays price is {price_as_float}.")
+    """
     message = f"""\
+    """
         Subject: Python Bot Price Tracking...
         To: {config('RECEIVER')}
         From: {config('SENDER')}
 
-        Amount is less than target price. Todays price is {price_as_float}."""
-
+        Amount is less than target price. Product title: {product_title.strip()}.Todays price is {price_as_float}."""
+    """
     try:
-        with smtplib.SMTP(config('HOST'), config('PORT')) as server:
+        with smtplib.SMTP('localhost') as server:
             server.login(config('USERNAME'), config('PASSWORD'))
             server.sendmail(config('SENDER'), config('RECEIVER'), message)
 
@@ -47,7 +52,8 @@ if price_as_float < float(config('TARGET_PRICE')):
     except smtplib.SMTPServerDisconnected:
         print("Failed to connect to the server. Wrong user/password?")        
     except smtplib.SMTPException as e:
-        print("SMTP error occured: " + str(e))       
+        print("SMTP error occured: " + str(e))    
+    """             
        
     
 
